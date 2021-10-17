04bce7e7cc4f7ce65f9c2bb01674a957896187d5
GitHub Action <action@github.com>
2021-10-17 12:09:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/alejandrorosas/intellijcodeownersplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alejandrorosas.intellijcodeownersplugin.services
+
+import com.github.alejandrorosas.intellijcodeownersplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
